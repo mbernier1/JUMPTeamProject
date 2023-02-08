80881584237990ce9b97e913ea9d27cbe7f9f8be
Mike <38372845+mbernier1@users.noreply.github.com>
2023-02-08 13:56:53 -0800

    Added pokemon queries

---
+++ queries/db_queries.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify, request
+import pokemon_data as pokemon
+
+
+
+app = Flask(__name__)
+
+
+# read all pokemon cards
+@app.route("/cards", methods=["GET"])
+def get_all_cards():
+    return jsonify(pokemon.read_all_cards())
+
+# Create a card
+@app.route("/cards", methods=["POST"])
+def create_a_card():
+    data = request.get_json()
+    pokemon.create_new_pokemon_card(data)
+    return "New pokemon added"
+    
+        
+
+# Read a card
+def read_a_card(card_name):
+    
+
+
+# Update a card
+def update_a_card(card_name):
+    
+
+
+# Delete a card
+def delete_a_card(card_name):
+    
