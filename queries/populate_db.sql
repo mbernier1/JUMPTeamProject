a711a7d1810aaa3a0a81bf31635c3dc6cce72f4e
MausVII <elanlocke@gmail.com>
2023-02-08 16:12:21 -0500

    some progress

---
+++ queries/populate_db.sql
@@ -0,0 +1,22 @@
+USE pokemon_db;
+
+INSERT INTO cards VALUES(
+    (1, 'Tepig', 0, 2, 70, 3.31),
+    (2, 'Victini', 0, 1, 60, 4.85),
+    (3, 'Ninetales', 1, 1, 90, 8.35),
+    (4, "Snivy", 0, 1, 60, 1.90),
+    (5, "Snivy", 0, 1, 60, 1.90),
+    (6, "Snivy", 0, 1, 60, 1.90),
+    (7, "Snivy", 0, 1, 60, 1.90),
+    (8, "Snivy", 0, 1, 60, 1.90),
+    (9, "Snivy", 0, 1, 60, 1.90),
+    (10, "Snivy", 0, 1, 60, 1.90),
+    (11, "Snivy", 0, 1, 60, 1.90),
+    (12, "Snivy", 0, 1, 60, 1.90),
+)
+
+INSERT INTO types VALUES(
+    (1, "Fire"),
+    (2, 'Grass'),
+    (3, "Water")
+)
