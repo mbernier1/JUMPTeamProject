5368cfaf1e7500ae4c8ad973433283a0782c6386
MausVII <elanlocke@gmail.com>
2023-02-08 16:38:38 -0500

    sample app skeleton

--- init.py
+++ init.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/cards')
+@app.route('/cards', methods=["GET"])
 def get_all_cards():
     return 'all cards'
+
+if __name__ == "__main__":
+    app.run(port=8080)
