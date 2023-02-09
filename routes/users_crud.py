cd24e17a08be7bf22320ef84c23dc743baa4c065
Mike <38372845+mbernier1@users.noreply.github.com>
2023-02-09 08:42:16 -0800

    Added more routes

--- routes/users_crud.py
+++ routes/users_crud.py
@@ -1,12 +1,20 @@
+from flask import Blueprint, request
+from database import db
+from misc.utilities import read_sq
 
-
-
+users_crud_blueprint = Blueprint('users_crud', __name__)
 
 
 
 
 # Add user to database
+@users_crud_blueprint.route('/users', method=["GET"])
+def add_user():
 
 # Update user in database
+@users_crud_blueprint.route('/users', method=["GET"])
+def update_user(name):
 
 # Delete user from database
+@users_crud_blueprint.route('/users', method=["GET"])
+def delete_user(name):
