cec5c25c4237729a6c37312ac72b51b707f19655
John Rielley <jrielley99@gmail.com>
2023-02-09 11:36:38 -0500

    Input validation

--- routes/cards.py
+++ routes/cards.py
@@ -14,23 +14,34 @@ def get_all_cards() -> list[dict]:
 
 @cards_blueprint.route("/cards/<name>", methods=["GET"])
 def get_card_by_id(name):
-    cur = db.new_cursor(dictionary=True)
-    cur.execute(read_sql("get_card_by_name"), [name])
-    cards = cur.fetchall()
-    return cards
+    try:
+        if type(name) != str:
+            raise ValueError
+        cur = db.new_cursor(dictionary=True)
+        cur.execute(read_sql("get_card_by_name"), [name])
+        cards = cur.fetchall()
+        return cards
+    except ValueError:
+        return "Invalid input for name", 422
 
 
 #--------------------------------------------------------------
 # I, Mike, added these, I hope they are right
+# I, John, also hope they are right
 
 
 # Function to query cards by type
 @cards_blueprint.route("/cards/<type>", methods=["GET"])
 def get_cards_by_type(type):
+    try:
+        if type(type) != str:
+            raise ValueError
         cur = db.new_cursor(dictionary=True)
         cur.execute(read_sql("get_card_by_type"), [type])
         cards_by_type = cur.fetchall()
         return cards_by_type
+    except ValueError:
+        return "Invalid input for type", 422
 
 
 # Function to query cards by hp
