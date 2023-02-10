731ceae971fa728699b24ed405fb0ddbe7ec33a1
John Rielley <jrielley99@gmail.com>
2023-02-10 11:23:54 -0500

    regex

--- routes/reviews.py
+++ routes/reviews.py
@@ -9,7 +9,7 @@ reviews_blueprint = Blueprint('reviews', __name__)
 @reviews_blueprint.route('/reviews/<cname>', methods=['GET'])
 def get_card_reviews(cname):
     try:
-        cid = int(cname)
+        cname = int(cname)
     except ValueError:
         return "Invalid card name", 422
     else:
@@ -22,7 +22,8 @@ def get_card_reviews(cname):
 @reviews_blueprint.route('/reviews/<email>', methods=['GET'])
 def get_card_reviews(email):
     try:
-        pass
+        if not re.fullmatch(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b', email):
+            raise ValueError
     except ValueError:
         return "Invalid email address", 422
     else:
